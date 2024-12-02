f3b610eecb73f2f714a361b09491e64394e71e87
Lian Shi <lians@student.chalmers.se>
2024-12-02 14:49:43 +0100

    #Fix naming

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -1,13 +1,11 @@
 import os
 
 
-def readNumbers(filename):
+def read_numbers(filename):
     first_list = []
     second_list = []
-    
     script_dir = os.path.dirname(__file__)
     filename = os.path.join(script_dir, filename)
-    
     with open (filename, "r") as file:
         for line in file:
             numbers = line.split() 
@@ -16,7 +14,7 @@ def readNumbers(filename):
     return first_list, second_list
 
 
-def sumSortedNumberDiff(lst1, lst2):
+def sum_sorted_number_diff(lst1, lst2):
     difference = 0
     lst1 = sorted(lst1)
     lst2 = sorted(lst2)
@@ -25,18 +23,18 @@ def sumSortedNumberDiff(lst1, lst2):
     print("The sum of distances between two sorted assay is "+ str(difference)) 
   
 
-def findSimilarity(lst1, lst2):
+def find_similarity(lst1, lst2):
     sum=0
     for i in range(len(lst1)):
         for j in range(len(lst2)):
             if lst1[i] == lst2[j]:
                 sum += lst1[i]
-
     print("The similarity is " + str(sum))
 
+
 print("real problem")
-lst1, lst2= readNumbers("day1.txt")
-sumSortedNumberDiff(lst1, lst2)
-findSimilarity(lst1, lst2)
+lst1, lst2= read_numbers("day1.txt")
+sum_sorted_number_diff(lst1, lst2)
+find_similarity(lst1, lst2)
 
 
